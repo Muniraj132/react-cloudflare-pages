438ddcbeda776bacfeec7bf295f95e1581d7cf85
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-09-11 17:45:27 +0530

    change url
    change url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       {/* Define your routes using the Routes component */}
       <Routes>
-        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Users />} />
         <Route path="/appuser" element={<Appuser />} />
         {/* Add more routes as needed */}
       </Routes>

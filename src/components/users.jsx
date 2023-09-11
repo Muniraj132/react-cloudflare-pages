a04b5b5cd67f864c1bc7f14ac514590ca7221bf3
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-09-11 18:16:36 +0530

    correct
    correct

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -447,7 +447,7 @@ const Users = ({ users }) => {
                     <i className="bx bx-file" />
                   </div>
                   <h4>
-                    <a href>Sed ut perspici</a>
+                    <a href="/">Sed ut perspici</a>
                   </h4>
                   <p>
                     Duis aute irure dolor in reprehenderit in voluptate velit
@@ -464,7 +464,7 @@ const Users = ({ users }) => {
                     <i className="bx bx-tachometer" />
                   </div>
                   <h4>
-                    <a href>Magni Dolores</a>
+                    <a href="/">Magni Dolores</a>
                   </h4>
                   <p>
                     Excepteur sint occaecat cupidatat non proident, sunt in
@@ -481,7 +481,7 @@ const Users = ({ users }) => {
                     <i className="bx bx-layer" />
                   </div>
                   <h4>
-                    <a href>Nemo Enim</a>
+                    <a href="/">Nemo Enim</a>
                   </h4>
                   <p>
                     At vero eos et accusamus et iusto odio dignissimos ducimus
@@ -506,7 +506,7 @@ const Users = ({ users }) => {
                 </p>
               </div>
               <div className="col-lg-3 cta-btn-container text-center">
-                <a className="cta-btn align-middle" href="#">
+                <a className="cta-btn align-middle" href="/">
                   Call To Action
                 </a>
               </div>

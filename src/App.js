389700b43d848faea7b9708cac623767265a6a69
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-09-11 17:28:39 +0530

    add router
    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { Component } from "react";
+// Import necessary dependencies
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Import your components
+
 import Users from "./components/users";
 import Appuser from "./components/usersexamplpe";
-class App extends Component {
-  state = {
-    users: [],
-  };
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ users: data });
-      })
-      .catch(console.log);
-  }
-
-  render() {
-    // return <Users />;
-    return <Appuser />;
-  }
+function App() {
+  return (
+    <div>
+      {/* Define your routes using the Routes component */}
+      <Routes>
+        <Route path="/users" element={<Users />} />
+        <Route path="/appuser" element={<Appuser />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

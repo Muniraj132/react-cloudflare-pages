4f8c200d8de99b25cb04dd096cab6bf2eb34f1d7
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-09-11 18:14:00 +0530

    Add sections
    Add sections

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -327,6 +327,192 @@ const Users = ({ users }) => {
             </div>
           </div>
         </section>
+        <section id="skills" className="skills">
+          <div className="container" data-aos="fade-up">
+            <div className="row">
+              <div
+                className="col-lg-6 d-flex align-items-center"
+                data-aos="fade-right"
+                data-aos-delay={100}>
+                <img src="assets/img/skills.png" className="img-fluid" alt="" />
+              </div>
+              <div
+                className="col-lg-6 pt-4 pt-lg-0 content"
+                data-aos="fade-left"
+                data-aos-delay={100}>
+                <h3>
+                  Voluptatem dignissimos provident quasi corporis voluptates
+                </h3>
+                <p className="fst-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className="skills-content">
+                  <div className="progress">
+                    <span className="skill">
+                      HTML <i className="val">100%</i>
+                    </span>
+                    <div className="progress-bar-wrap">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        aria-valuenow={100}
+                        aria-valuemin={0}
+                        aria-valuemax={100}
+                      />
+                    </div>
+                  </div>
+                  <div className="progress">
+                    <span className="skill">
+                      CSS <i className="val">90%</i>
+                    </span>
+                    <div className="progress-bar-wrap">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        aria-valuenow={90}
+                        aria-valuemin={0}
+                        aria-valuemax={100}
+                      />
+                    </div>
+                  </div>
+                  <div className="progress">
+                    <span className="skill">
+                      JavaScript <i className="val">75%</i>
+                    </span>
+                    <div className="progress-bar-wrap">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        aria-valuenow={75}
+                        aria-valuemin={0}
+                        aria-valuemax={100}
+                      />
+                    </div>
+                  </div>
+                  <div className="progress">
+                    <span className="skill">
+                      Photoshop <i className="val">55%</i>
+                    </span>
+                    <div className="progress-bar-wrap">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        aria-valuenow={55}
+                        aria-valuemin={0}
+                        aria-valuemax={100}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="services" className="services section-bg">
+          <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>Services</h2>
+              <p>
+                Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+                ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+                quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+                Quia fugiat sit in iste officiis commodi quidem hic quas.
+              </p>
+            </div>
+            <div className="row">
+              <div
+                className="col-xl-3 col-md-6 d-flex align-items-stretch"
+                data-aos="zoom-in"
+                data-aos-delay={100}>
+                <div className="icon-box">
+                  <div className="icon">
+                    <i className="bx bxl-dribbble" />
+                  </div>
+                  <h4>
+                    <a href>Lorem Ipsum</a>
+                  </h4>
+                  <p>
+                    Voluptatum deleniti atque corrupti quos dolores et quas
+                    molestias excepturi
+                  </p>
+                </div>
+              </div>
+              <div
+                className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0"
+                data-aos="zoom-in"
+                data-aos-delay={200}>
+                <div className="icon-box">
+                  <div className="icon">
+                    <i className="bx bx-file" />
+                  </div>
+                  <h4>
+                    <a href>Sed ut perspici</a>
+                  </h4>
+                  <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore
+                  </p>
+                </div>
+              </div>
+              <div
+                className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+                data-aos="zoom-in"
+                data-aos-delay={300}>
+                <div className="icon-box">
+                  <div className="icon">
+                    <i className="bx bx-tachometer" />
+                  </div>
+                  <h4>
+                    <a href>Magni Dolores</a>
+                  </h4>
+                  <p>
+                    Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia
+                  </p>
+                </div>
+              </div>
+              <div
+                className="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0"
+                data-aos="zoom-in"
+                data-aos-delay={400}>
+                <div className="icon-box">
+                  <div className="icon">
+                    <i className="bx bx-layer" />
+                  </div>
+                  <h4>
+                    <a href>Nemo Enim</a>
+                  </h4>
+                  <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus
+                    qui blanditiis
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="cta" className="cta">
+          <div className="container" data-aos="zoom-in">
+            <div className="row">
+              <div className="col-lg-9 text-center text-lg-start">
+                <h3>Call To Action</h3>
+                <p>
+                  {" "}
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia
+                  deserunt mollit anim id est laborum.
+                </p>
+              </div>
+              <div className="col-lg-3 cta-btn-container text-center">
+                <a className="cta-btn align-middle" href="#">
+                  Call To Action
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );

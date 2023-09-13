75d9d47988b2ffb15b54e8251f7db2b3c8614efe
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-09-13 12:18:55 +0530

    remove the content
    remove the content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,20 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
 function Footer() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <>
       <footer id="footer">
